354086d82b5ddf126975ee1c322767b25ac7cb45
elijah-villanu <elijahred9@outlook.com>
2024-10-10 14:54:35 -0700

    Fixed bottler delivery
    Changed the return of the bottle delivery statement to return actual potion stock rather than barrel ml.

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -46,36 +46,40 @@ def get_bottle_plan():
         b_ml = connection.execute(sqlalchemy.text("SELECT num_blue_ml FROM global_inventory")).scalar()
         b_stock = connection.execute(sqlalchemy.text("SELECT num_blue_potions FROM global_inventory")).scalar()
         
-
+        r_made, g_made, b_made = 0
     #Checks if there is enough in barrel, then bottles it
+    #Use modulus floor
         while r_ml >= 100:
+            r_made += 1
             r_stock += 1
             r_ml = r_ml - 100
 
         while g_ml >= 100:
+            g_made += 1
             g_stock += 1
             g_ml = g_ml - 100
         
         while b_ml >= 100:
+            b_made += 1
             b_stock += 1
             b_ml = b_ml - 100
 
-        
+        #When updating potion quantity, make sure adding, not overiding
         connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_red_potions = '{r_stock}', num_red_ml = '{r_ml}' WHERE id = 1"))
         connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_green_potions = '{g_stock}', num_green_ml = '{g_ml}' WHERE id = 1"))
         connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_blue_potions = '{b_stock}', num_blue_ml = '{b_ml}' WHERE id = 1"))            
     return [
             {
                 "potion_type": [100, 0, 0, 0],
-                "quantity": r_ml,
+                "quantity": r_made,
             },
             {
                 "potion_type": [0, 100, 0, 0],
-                "quantity": g_ml,
+                "quantity": g_made,
             },
             {
                 "potion_type": [0, 0, 100, 0],
-                "quantity": b_ml
+                "quantity": b_made
             }
         ]
 
