48b0c145936a60049361ecf8801a08c4a66b13ed
elijah-villanu <elijahred9@outlook.com>
2024-11-11 11:37:35 -0800

    Update catalog.py

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -14,15 +14,23 @@ def get_catalog():
 
     catalog = []
     with db.engine.begin() as connection:
-        potions = connection.execute(sqlalchemy.text("SELECT * FROM POTIONS")).fetchall()
+        potions_query = """
+                        SELECT p.potion_sku, p.name, COALESCE(SUM(pl.quantity), 0) AS quantity,
+                            p.price, p.rgbd 
+                        FROM potions AS p
+                        LEFT JOIN potion_ledger AS pl
+                            ON pl.item_sku = p.potion_sku
+                        GROUP BY p.potion_sku,p.name, p.rgbd, p.price
+                        """
+        potions = connection.execute(sqlalchemy.text(potions_query))
+        
         for row in potions:
-            type = [row[8]]
             catalog.append({
-                "sku": row[1],
-                "name": row[2],
-                "quantity": row[3],
-                "price": row[4],
-                "potion_type": type,
+                "sku": row.potion_sku,
+                "name": row.name,
+                "quantity": row.quantity,
+                "price": row.price,
+                "potion_type": row.rgbd,
             })
 
     return catalog
